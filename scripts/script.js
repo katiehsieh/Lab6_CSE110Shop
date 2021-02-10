955b8c4c9eabf70643f2816f10baf6bf8ab5f7b8
Katie Hsieh <katiehsieh25@gmail.com>
2021-02-09 21:44:16 -0800

    fetch test

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+      localStorage.setItem('data', JSON.stringify(data))
+  });
 });
