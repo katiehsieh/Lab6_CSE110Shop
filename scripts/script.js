76e770598be5af8ef8f0793ab11c3267edd845a0
Katie Hsieh <katiehsieh25@gmail.com>
2021-02-12 21:42:59 -0800

    check fetch test

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => {
-      localStorage.setItem('data', JSON.stringify(data))
-  });
+  if (localStorage.getItem('data') === null) {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => {
+        localStorage.setItem('data', JSON.stringify(data))
+      });
+  }
 });
