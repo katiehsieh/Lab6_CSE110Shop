fb1839e823abdf8ae325687cc3b5c853f2359ea6
Katie Hsieh <katiehsieh25@gmail.com>
2021-02-12 23:40:53 -0800

    set items test

--- components/product-item.js
+++ components/product-item.js
@@ -73,9 +73,9 @@ class ProductItem extends HTMLElement {
       </style>
 
       <li class="product">
-        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
-        <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
-        <p class="price">$109.95</p>
+        <img id="image" src="" alt="image" width=200>
+        <p id="title" class="title">title</p>
+        <p id="price" class="price">price</p>
         <button onclick="alert('Added to Cart!')">Add to Cart</button>
       </li>
     `;
